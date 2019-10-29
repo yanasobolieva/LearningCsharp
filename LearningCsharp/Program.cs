bba2f1b4230fd3c0d875cd510b2fa17db300fd67
yanasobolieva <soboleva.yana.sys@gmail.com>
2019-10-30 01:18:09 +0200

    Lesson 3 homework

--- LearningCsharp/Program.cs
+++ LearningCsharp/Program.cs
@@ -1,69 +1,28 @@
 ﻿using System;
 
-namespace LearningCsharp.Lesson1
+namespace LearningCsharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CheckIfDigitIsEven();
-            CalculateDistance();
-            CalculateResistance();
-            FindMaxValue();
-            Console.ReadKey();
-        }
-
-        static void CheckIfDigitIsEven()
-        {
-            Console.WriteLine("Please, enter some digit and press Enter:");
-            int someDigit = Convert.ToInt32(Console.ReadLine());
-            EvenCheck evenCheck = new EvenCheck();
-            if (evenCheck.IsEven(someDigit))
+            Student jack = new Student("Jack", "Unknown");
+            jack.AddMark(5);
+            jack.AddMark(4);
+            jack.AddMark(-4); // в массив такое число не добавляем.
+            int sum = 0;
+            foreach (var mark in jack.GetMarks())
             {
-                Console.WriteLine("You have entered an even digit!");
-            }
-            else
-            {
-                Console.WriteLine("You have entered an odd digit!");
-            }
-        }
-
-        static void CalculateDistance()
-        {
-            Console.WriteLine("Please, enter the distance to the airport in kilometers and press Enter:");
-            double distance = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please, enter the time in hours and press Enter:");
-            double time = Convert.ToDouble(Console.ReadLine());
-            SpeedCounter speedCounter = new SpeedCounter();
-            double speed = speedCounter.CountDriverSpeed(distance, time);
-            Console.WriteLine($"You need to drive with speed '{speed} km/h'");
-        }
-
-        static void CalculateResistance()
-        {
-            ResistanceCounter resistanceCounter = new ResistanceCounter();
-            Console.WriteLine($"The resistance R0 value is '{resistanceCounter.CountResistance(2, 4, 8)}' \n");
-        }
-
-        static void FindMaxValue()
-        {
-            bool firstInput = true;
-            int maxValue = 0;
-            for (int i = 0; i < 7; i++)
-            {
-                var currentEnteredValue = Convert.ToInt32(Console.ReadLine());
-                if (firstInput)
-                {
-                    maxValue = currentEnteredValue;
-                    firstInput = false;
-                }
-                else if (currentEnteredValue > maxValue)
-                {
-                    maxValue = currentEnteredValue;
-                }
+                sum += mark;
             }
 
-            Console.WriteLine(maxValue);
+            Console.WriteLine(jack.GetMarksQuantity());
+            double avg = sum / (double)jack.GetMarksQuantity();
+            Console.WriteLine($"{jack.Name} {jack.Surname} has average mark: {avg}");
+
+            // Console.WriteLine(jack.Marks.Length);
+            // double avg = sum / (double)jack.Marks.Length;
+            // Console.WriteLine($"{jack.Name} {jack.Surname} has average mark: {avg}");
         }
     }
-}
+}    
---
+++ LearningCsharp/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCsharp
+{
+    public class Student
+    {
+        public String Name { get; private set; }
+        public String Surname { get; private set; }
+        public int[] Marks { get; private set; }
+        private int StartCount = 0;
+
+        public Student(string firstName, string lastName) 
+        {
+            Name = firstName;
+            Surname = lastName;
+            Marks = new int[1];
+        }
+
+        public void AddMark(int mark)
+        {
+            if (mark >= 1 && mark <= 12)
+            {
+                Marks[StartCount] = mark;
+                StartCount++;
+
+                if (Marks.Length == StartCount)
+                {
+                    int[] temporaryMarks = new int[(Marks.Length * 2)];
+                    Array.Copy(Marks, temporaryMarks, Marks.Length);
+                    Marks = temporaryMarks;
+                }
+            }           
+        }
+         
+        public int[] GetMarks()
+        {
+            return Marks;
+        }
+        public int GetMarksQuantity()
+        {
+            return StartCount;
+        }
+    }
+}
